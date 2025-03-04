76d79b0c629166997c5a43c3aee853391e0df487
Midhru Jayan J <midhru004@gmail.com>
2025-03-04 23:38:28 +0530

    Sample processing on skin dataset

--- GAN Basics/Basic_GAN.py
+++ GAN Basics/Basic_GAN.py
@@ -6,3 +6,20 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
+ 
+ 
+class Discriminator(nn.Module):
+    def __init__(self,img_dim):
+        super().__init__()
+        self.disc =nn.Sequential(                           
+            nn.Linear(img_dim,128),
+            nn.LeakyReLU(img_dim,128),
+            nn.Linear(128,1),
+            nn.Sigmoid(),         
+        )
+        
+        def forward(self,x):
+            return self.disc(x)
+        
+        
+        
